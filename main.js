e80007825905d86bb2ba4035cfc13bf97c0ad23a
vincentYoung <vinni.xander@gmail.com>
2022-12-16 12:09:23 -0700

    Add basic blog comment form functionality
    Should have committed more frequently but now have a working form
    without any major XSS issues.
    
    Improvements to come

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// mobile navigation functionality
+
 const navToggle = document.querySelector('.toggle-button');
 const navLinks = document.querySelector('.nav-links');
 
@@ -5,4 +7,50 @@ navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-hidden')
 })
 
-// add event handler for keyboard users
+// blog comment form functionality
+
+const commentForm = document.querySelector('.comment-form');
+const commentsSection = document.querySelector('.comments-section');
+
+function getCommentDetails() {
+    const userName = document.querySelector('#user-name').value;
+    const userComment = document.querySelector('#user-comment').value;
+    return { userName, userComment };
+}
+
+function postCommentDetails(commentObject) {
+    const containerEl = createCommentContainerEl();
+    const nameEl = containerEl.querySelector('.user-name');
+    nameEl.textContent = `${commentObject.userName}`
+    const commentEl = containerEl.querySelector('.user-comment');
+    commentEl.textContent = `${commentObject.userComment}`;
+    commentsSection.insertBefore(containerEl, commentForm);
+}
+
+function createCommentContainerEl() {
+    const container = document.createElement('article');
+    container.classList.add('comment');
+    container.innerHTML = `<div class="visitor-img">
+                                <img src="./assets/comment-2.jpg" alt="Julia's profile picture">
+                            </div>
+                            <div class="visitor-commment">
+                                <p>${createDateString()} by <span class="user-name"></span></p>
+                                <p class="user-comment"></p>
+                            </div> <!-- .visitor-comment end -->`;
+    return container;
+}
+
+function createDateString() {
+    const date = new Date();
+    const weekDay = date.toLocaleString('en-us', { weekday: 'long' });
+    const monthDay = date.getDate();
+    const month = date.toLocaleString('en-us', { month: 'long' });
+    const year = date.getFullYear();
+    return `${weekDay} ${month} ${monthDay}th, ${year}`
+}
+
+commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const commentDetails = getCommentDetails();
+    postCommentDetails(commentDetails);
+})
